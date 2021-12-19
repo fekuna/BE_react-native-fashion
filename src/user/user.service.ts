d4c16b327efdc1f9753b1924030e8c11c41503a5
Alfan Almunawar <almunawar.alfan@gmail.com>
2021-12-20 01:32:01 +0700

    AutoIncrement as Primary key roles'

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -52,7 +52,7 @@ export class UserService {
       name: data.name,
       password: data.hashPassword,
       role: {
-        id: '5d3a036e-3644-40af-a575-b2bdfa1aa13f',
+        id: 1,
       },
     });
 
@@ -65,7 +65,7 @@ export class UserService {
     await this.userRepository.update(id, {
       ...data,
       role: {
-        id: '5d3a036e-3644-40af-a575-b2bdfa1aa13f',
+        id: 1,
       },
     });
   }
