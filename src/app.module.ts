9a3987b4ca1573c57eb2cae7ede94fbcb4895fb3
Alfan Almunawar <almunawar.alfan@gmail.com>
2021-12-20 17:23:51 +0700

    CHECKPOINT module upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { ProductModule } from './product/product.module';
 import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
 import { OrderModule } from './order/order.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { OrderModule } from './order/order.module';
     RoleModule,
     PermissionModule,
     OrderModule,
+    UploadModule,
   ],
   providers: [
     {
